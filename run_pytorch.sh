86f2850e9c572b8bfd7a687af1dcc45a08a48f37
Akifumi Goto <souldrive7@gmail.com>
2026-04-14 15:28:33 +0900

    Use recommended PyTorch container flags

--- run_pytorch.sh
+++ run_pytorch.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 docker run --rm --gpus all -it \
+  --ipc=host \
+  --ulimit memlock=-1 \
+  --ulimit stack=67108864 \
   -e NVIDIA_API_KEY="$NVIDIA_API_KEY" \
   -v $PWD:/workspace \
   -w /workspace \
